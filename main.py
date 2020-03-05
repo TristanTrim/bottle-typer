f875e1b601a9e184c2d5842e86ebc866efb271cb
Tristan Trim <tTrim@linamar.com>
2020-03-05 11:32:12 -0500

    this is silly

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 
-num_char=2
-fl = open("Quotables/author-quote.txt",'r',encoding="utf8")
-fl.seek(0)
 from collections import Counter
-c=Counter()
-for qi in range(1000):
-    ln = fl.readline()
-    q = ln.split("\t")[1]
-    for i in range(len(q)-num_char+1):
-        c[q[i:i+num_char]]+=1
+fl = open("Quotables/author-quote.txt",'r',encoding="utf8")
 
-for k, v in reversed(sorted(c.items(), key=lambda item: item[1])):
-    if(v>50):
-        print("{}{}".format(k,"*"*int(v/100)))
+num_char=1
+for num_char in range(1,8):
+    fl.seek(0)
+    print("\n{}glyph frequencies:".format(num_char))
+    c=Counter()
+    for qi in range(10000):
+        ln = fl.readline()
+        q = ln.split("\t")[1]
+        for i in range(len(q)-num_char+1):
+            chrs = q[i:i+num_char]
+            #if(chrs.find(" ")==-1): #no spaces
+            c[chrs]+=1
+
+    divver = 100
+    for i,(k,v) in enumerate(reversed(sorted(c.items(), key=lambda item: item[1]))):
+        if(i==0):
+            divver = v/70
+        if(i==50):
+            break
+        print("{}|{}|{}".format(k,v,"*"*int(v/divver)))
