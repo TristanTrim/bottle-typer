ef278c63732e798b83f70a3020e8c5f4f667d3d9
Tristan Trim <tTrim@linamar.com>
2020-03-05 15:52:53 -0500

    playing with python frequency counter a bit. Seeing that some combinations of letters are more common than other letters on their own is interesting.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from collections import Counter
 fl = open("Quotables/author-quote.txt",'r',encoding="utf8")
 
 num_char=1
-for num_char in range(1,8):
+c=Counter()
+for num_char in range(2,8):
     fl.seek(0)
     print("\n{}glyph frequencies:".format(num_char))
-    c=Counter()
-    for qi in range(10000):
+    #c=Counter()
+    for qi in range(1000):
         ln = fl.readline()
         q = ln.split("\t")[1]
         for i in range(len(q)-num_char+1):
@@ -15,10 +16,10 @@ for num_char in range(1,8):
             #if(chrs.find(" ")==-1): #no spaces
             c[chrs]+=1
 
-    divver = 100
-    for i,(k,v) in enumerate(reversed(sorted(c.items(), key=lambda item: item[1]))):
-        if(i==0):
-            divver = v/70
-        if(i==50):
-            break
-        print("{}|{}|{}".format(k,v,"*"*int(v/divver)))
+divver = 100
+for i,(k,v) in enumerate(reversed(sorted(c.items(), key=lambda item: item[1]))):
+    if(i==0):
+        divver = v/70
+    if(i==130):
+        break
+    print("{}|{}|{}".format(k,v,"*"*int(v/divver)))
