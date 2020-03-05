bd253f5497368fa93a774454da7c20f423a701e5
Tristan Trim <tTrim@linamar.com>
2020-03-05 15:45:37 -0500

    this is neat. Not sure if how deep restructure it will need to become and actual thing, but at least cobbling together relevent knowhow in one place.

--- bottle-typer.js
+++ bottle-typer.js
@@ -2,25 +2,40 @@
 function initialize(){
 	window.keybox = document.getElementById("keybox");
 	//window.keys = ['a','b','c'];
-	window.keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "i", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	//window.keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "i", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	window.keys = ["est","ing","and","ion"];
 	window.keySpeeds = {"a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, "l":0, "m":0, "n":0, "i":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0};
 	window.randomKey = 'nAn'
 	window.lastTime=0;
+	window.keybuf=""
 }
 function handle_keydown(e){
+	keybuf+=e.key;
+	keybox.innerHTML+=e.key;
+	console.log(randomKey,keybuf);
 	if(e.key == "Enter"){
 		console.log("starting");
 		randomKey = keys[Math.floor(Math.random() * keys.length)];
-		keybox.innerHTML=randomKey;
+		keybox.innerHTML=randomKey+"<br>";
 		window.lastTime=(new Date()).getTime()
+		keybuf = "";
+	}else if(e.key == "Backspace"){
+		e.preventDefault();// do not navigate browser back!
+		keybuf=keybuf.substr(0,keybuf.length-10)
+		keybox.innerHTML=keybox.innerHTML.substr(0,keybox.innerHTML.length-10)
 	}
-	if(randomKey === e.key ){
+	if(randomKey === keybuf ){
 		time=(new Date()).getTime();
 		interval = time-lastTime;
 		lastTime=time;
-		console.log(e.key, interval);
-		randomKey = keys[Math.floor(Math.random() * keys.length)];
-		keybox.innerHTML=randomKey;
+
+		console.log(keybuf, interval);
+
+		while(randomKey === keybuf){
+			randomKey = keys[Math.floor(Math.random() * keys.length)];
+		}
+		keybox.innerHTML=randomKey+"<br>";
+		keybuf = "";
 	}
 }
 
