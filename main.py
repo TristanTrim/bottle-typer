753bbf588f96866d67a64d317e770181acbfcc7c
TristanTrim <TristanTrim@gmail.com>
2022-05-19 13:52:07 -0400

    yup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ fl = open("Quotables/author-quote.txt",'r',encoding="utf8")
 
 num_char=1
 c=Counter()
-for num_char in range(2,8):
+for num_char in range(3,8):
     fl.seek(0)
     print("\n{}glyph frequencies:".format(num_char))
     #c=Counter()
